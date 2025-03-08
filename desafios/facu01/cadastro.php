8c1057d56c66e883c82d527cde5d81e7792917c3
JoelLopes949 <joelyalopes2710@gmail.com>
2025-03-08 20:14:26 -0300

    detalles

--- desafios/facu01/cadastro.php
+++
@@ -1,3 +0,0 @@
-Seu pedido foi entregue com sucesso.!!
-Aguarde um momento só e nossa equipe lhe enviará um e-mail com todos os detalhes do pagamento e entrega.!!
-Muito obrigado pela preferência. Tenha um ótimo dia.!!
